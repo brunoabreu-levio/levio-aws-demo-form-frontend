a1b39836c0f58079dc556859faf83c129d9a5a0b
brunoabreu-levio <bruno.abreu@levio.ca>
2024-01-18 08:57:25 -0500

    FR/EN language

--- js/app.js
+++ js/app.js
@@ -36,12 +36,6 @@ document.addEventListener('DOMContentLoaded', function () {
         span.classList.add('error');
     }
 
-    function clearErrorMessage(inputElement) {
-        const span = inputElement.nextElementSibling;
-        span.innerText = '';
-        span.classList.remove('error');
-    }
-
     function validateForm() {
         let isValid = true;
         const inputs = form.querySelectorAll('[data-val="true"]');
@@ -98,12 +92,85 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     }
 
-    submitButton.addEventListener('click', function (event) {
-        event.preventDefault();
-        if (validateForm()) {
-            submitForm();
-        } else {
-            console.log("Invalid form!!!")
-        }
-    });
+    if (submitButton) {
+        submitButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (validateForm()) {
+                submitForm();
+            } else {
+                console.log("Invalid form!!!")
+            }
+        });
+    }
 });
+
+function setLanguage(lang) {
+    clearErrorMessages();
+    document.querySelectorAll("[data-key]").forEach(elem => {
+            const key = elem.getAttribute("data-key");
+            const langData = languages[lang][key];
+
+            if (elem.tagName === 'A') {
+                if (langData['url']) {
+                    elem.href = langData['url'];
+                }
+                if (langData['title']) {
+                    elem.title = langData['title'];
+                }
+                if (langData['text']) {
+                    elem.innerHTML  = langData['text'];
+                }
+            } else if (elem.tagName === 'INPUT') {
+                if (langData['text']) {
+                    elem.placeholder = langData['text'];
+                }
+                if (langData['data-val-required']) {
+                    elem.setAttribute('data-val-required', langData['data-val-required']);
+                }
+                if (langData['data-val-phone']) {
+                    elem.setAttribute('data-val-phone', langData['data-val-phone']);
+                }
+                if (langData['data-val-email']) {
+                    elem.setAttribute('data-val-email', langData['data-val-email']);
+                }
+            } else {
+                elem.innerHTML  = langData;
+            }
+        }
+    );
+
+    const baseClass = "group h-extend-cursor-area text-white uppercase relative rounded-md h-8 w-9 flex justify-center items-center h-focus-state h-focus-state--offset-primary";
+    const clickedClass = baseClass + " text-primary-500 bg-white font-extrabold cursor-default";
+    const notClickedClass = baseClass + " pf-transition-link-underline-enter";
+
+    const langFr = document.getElementById('langFr');
+    const langEn = document.getElementById('langEn');
+
+    if (lang === 'fr') {
+        langFr.className = clickedClass;
+        langEn.className = notClickedClass;
+    } else if (lang === 'en') {
+        langEn.className = clickedClass;
+        langFr.className = notClickedClass;
+    }
+}
+
+function clearErrorMessage(inputElement) {
+    const span = inputElement.nextElementSibling;
+    span.innerText = '';
+    span.classList.remove('error');
+}
+
+function clearErrorMessages() {
+    const form = document.getElementById('contactForm');
+    if (form) {
+        const inputs = form.querySelectorAll('[data-val="true"]');
+
+        inputs.forEach(input => {
+            clearErrorMessage(input);
+        });
+    }
+}
+
+setLanguage('fr');
+
